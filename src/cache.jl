8c77c820f964e2c831bf3814016ade577c237337
Thomas Burton <tburton5572@gmail.com>
2022-01-09 14:58:34 +0000

    added methods to update cached derivatives and pressure

--- src/cache.jl
+++ src/cache.jl
@@ -103,8 +103,8 @@ function (f::Cache{T, S})(U::V) where {T, S, V<:AbstractVector{S}}
     dvdy_dwdz = f.phys_cache[16]
 
     # assign transform aliases
-    FFT = f.plans[1]
-    IFFT = f.plans[2]
+    FFT! = f.plans[1]
+    IFFT! = f.plans[2]
 
     # compute derivatives
     ddt!(U[1], dUdt)
@@ -124,14 +124,14 @@ function (f::Cache{T, S})(U::V) where {T, S, V<:AbstractVector{S}}
     d2dy2!(U[3], d2Wdy2)
 
     # compute nonlinear components
-    IFFT(v, U[2], ifft_tmp1)
-    IFFT(w, U[3], ifft_tmp2)
-    IFFT(dudz, dUdz, ifft_tmp3)
-    IFFT(dvdz, dVdz, ifft_tmp4)
-    IFFT(dwdz, dWdz, ifft_tmp5)
-    IFFT(dudy, dUdy, ifft_tmp6)
-    IFFT(dvdy, dVdy, ifft_tmp7)
-    IFFT(dwdy, dWdy, ifft_tmp8)
+    IFFT!(v, U[2], ifft_tmp1)
+    IFFT!(w, U[3], ifft_tmp2)
+    IFFT!(dudz, dUdz, ifft_tmp3)
+    IFFT!(dvdz, dVdz, ifft_tmp4)
+    IFFT!(dwdz, dWdz, ifft_tmp5)
+    IFFT!(dudy, dUdy, ifft_tmp6)
+    IFFT!(dvdy, dVdy, ifft_tmp7)
+    IFFT!(dwdy, dWdy, ifft_tmp8)
     v_dudy .= v.*dudy
     w_dudz .= w.*dudz
     v_dvdy .= v.*dvdy
@@ -140,14 +140,14 @@ function (f::Cache{T, S})(U::V) where {T, S, V<:AbstractVector{S}}
     w_dwdz .= w.*dwdz
     dvdz_dwdy .= dvdz.*dwdy
     dvdy_dwdz .= dvdy.*dwdz
-    FFT(V_dUdy, v_dudy)
-    FFT(W_dUdz, w_dudz)
-    FFT(V_dVdy, v_dvdy)
-    FFT(W_dVdz, w_dvdz)
-    FFT(V_dWdy, v_dwdy)
-    FFT(W_dWdz, w_dwdz)
-    FFT(dVdz_dWdy, dvdz_dwdy)
-    FFT(dVdy_dWdz, dvdy_dwdz)
+    FFT!(V_dUdy, v_dudy)
+    FFT!(W_dUdz, w_dudz)
+    FFT!(V_dVdy, v_dvdy)
+    FFT!(W_dVdz, w_dvdz)
+    FFT!(V_dWdy, v_dwdy)
+    FFT!(W_dWdz, w_dwdz)
+    FFT!(dVdz_dWdy, dvdz_dwdy)
+    FFT!(dVdy_dWdz, dvdy_dwdz)
 
     # compute rhs of pressure equation
     poiss_rhs .= -2.0.*(dVdz_dWdy .- dVdy_dWdz) .- f.Ro.*dUdy
@@ -165,5 +165,138 @@ function (f::Cache{T, S})(U::V) where {T, S, V<:AbstractVector{S}}
     ddy!(Pr, dPdy)
     ddz!(Pr, dPdz)
 
-    return nothing
+    return
+end
+
+function _update_vel!(U::V, cache::Cache{T, S}) where {T, S, V<:AbstractVector{S}}
+    # assign spectral aliases
+    dUdt = cache.spec_cache[1]
+    dVdt = cache.spec_cache[2]
+    dWdt = cache.spec_cache[3]
+    dUdz = cache.spec_cache[4]
+    dVdz = cache.spec_cache[5]
+    dWdz = cache.spec_cache[6]
+    d2Udz2 = cache.spec_cache[7]
+    d2Vdz2 = cache.spec_cache[8]
+    d2Wdz2 = cache.spec_cache[9]
+    dUdy = cache.spec_cache[10]
+    dVdy = cache.spec_cache[11]
+    dWdy = cache.spec_cache[12]
+    d2Udy2 = cache.spec_cache[13]
+    d2Vdy2 = cache.spec_cache[14]
+    d2Wdy2 = cache.spec_cache[15]
+    V_dUdy = cache.spec_cache[20]
+    W_dUdz = cache.spec_cache[21]
+    V_dVdy = cache.spec_cache[22]
+    W_dVdz = cache.spec_cache[23]
+    V_dWdy = cache.spec_cache[24]
+    W_dWdz = cache.spec_cache[25]
+    dVdz_dWdy = cache.spec_cache[26]
+    dVdy_dWdz = cache.spec_cache[27]
+    ifft_tmp1 = cache.spec_cache[28]
+    ifft_tmp2 = cache.spec_cache[29]
+    ifft_tmp3 = cache.spec_cache[30]
+    ifft_tmp4 = cache.spec_cache[31]
+    ifft_tmp5 = cache.spec_cache[32]
+    ifft_tmp6 = cache.spec_cache[33]
+    ifft_tmp7 = cache.spec_cache[34]
+    ifft_tmp8 = cache.spec_cache[35]
+
+    # assign physical aliases
+    v = cache.phys_cache[1]
+    w = cache.phys_cache[2]
+    dudz = cache.phys_cache[3]
+    dvdz = cache.phys_cache[4]
+    dwdz = cache.phys_cache[5]
+    dudy = cache.phys_cache[6]
+    dvdy = cache.phys_cache[7]
+    dwdy = cache.phys_cache[8]
+    v_dudy = cache.phys_cache[9]
+    w_dudz = cache.phys_cache[10]
+    v_dvdy = cache.phys_cache[11]
+    w_dvdz = cache.phys_cache[12]
+    v_dwdy = cache.phys_cache[13]
+    w_dwdz = cache.phys_cache[14]
+    dvdz_dwdy = cache.phys_cache[15]
+    dvdy_dwdz = cache.phys_cache[16]
+
+    # assign transform aliases
+    FFT! = cache.plans[1]
+    IFFT! = cache.plans[2]
+
+    # compute derivatives
+    ddt!(U[1], dUdt)
+    ddt!(U[2], dVdt)
+    ddt!(U[3], dWdt)
+    ddz!(U[1], dUdz)
+    ddz!(U[2], dVdz)
+    ddz!(U[3], dWdz)
+    d2dz2!(U[1], d2Udz2)
+    d2dz2!(U[2], d2Vdz2)
+    d2dz2!(U[3], d2Wdz2)
+    ddy!(U[1], dUdy)
+    ddy!(U[2], dVdy)
+    ddy!(U[3], dWdy)
+    d2dy2!(U[1], d2Udy2)
+    d2dy2!(U[2], d2Vdy2)
+    d2dy2!(U[3], d2Wdy2)
+
+    # compute nonlinear components
+    IFFT!(v, U[2], ifft_tmp1)
+    IFFT!(w, U[3], ifft_tmp2)
+    IFFT!(dudz, dUdz, ifft_tmp3)
+    IFFT!(dvdz, dVdz, ifft_tmp4)
+    IFFT!(dwdz, dWdz, ifft_tmp5)
+    IFFT!(dudy, dUdy, ifft_tmp6)
+    IFFT!(dvdy, dVdy, ifft_tmp7)
+    IFFT!(dwdy, dWdy, ifft_tmp8)
+    v_dudy .= v.*dudy
+    w_dudz .= w.*dudz
+    v_dvdy .= v.*dvdy
+    w_dvdz .= w.*dvdz
+    v_dwdy .= v.*dwdy
+    w_dwdz .= w.*dwdz
+    dvdz_dwdy .= dvdz.*dwdy
+    dvdy_dwdz .= dvdy.*dwdz
+    FFT!(V_dUdy, v_dudy)
+    FFT!(W_dUdz, w_dudz)
+    FFT!(V_dVdy, v_dvdy)
+    FFT!(W_dVdz, w_dvdz)
+    FFT!(V_dWdy, v_dwdy)
+    FFT!(W_dWdz, w_dwdz)
+    FFT!(dVdz_dWdy, dvdz_dwdy)
+    FFT!(dVdy_dWdz, dvdy_dwdz)
+
+    return
+end
+
+function _update_p!(U::V, cache::Cache{T, S}) where {T, S, V<:AbstractVector{S}}
+    # assign aliases
+    dUdy = cache.spec_cache[10]
+    d2Vdy2 = cache.spec_cache[14]
+    P = cache.spec_cache[16]
+    dPdy = cache.spec_cache[17]
+    dPdz = cache.spec_cache[18]
+    poiss_rhs = cache.spec_cache[19]
+    dVdz_dWdy = cache.spec_cache[26]
+    dVdy_dWdz = cache.spec_cache[27]
+
+    # compute rhs of pressure equation
+    poiss_rhs .= -2.0.*(dVdz_dWdy .- dVdy_dWdz) .- cache.Ro.*dUdy
+    # poiss_rhs .= dVdz_dWdy .- dVdy_dWdz
+
+    # extract boundary condition data
+    @views begin
+        cache.bc_cache[1] .= cache.Re_recip.*d2Vdy2[1, :, :]
+        cache.bc_cache[2] .= cache.Re_recip.*d2Vdy2[end, :, :]
+    end
+
+    # solve the pressure equation
+    solve!(P, cache.lapl, poiss_rhs, cache.bc_cache)
+
+    # compute pressure gradients
+    ddy!(P, dPdy)
+    ddz!(P, dPdz)
+
+    return
 end
--- test/test_cache.jl
+++ test/test_cache.jl
@@ -36,3 +36,198 @@
     @test_throws MethodError Cache(U, u, rand(Int, Ny), dūdy, d2ūdy2, dp̄dy, Re, Ro)
     @test_throws MethodError Cache(U, u, ū, dūdy, d2ūdy2, dp̄dy, 1+1im, Ro)
 end
+
+@testset "Cache velocity update         " begin
+    # initialise incompressible velocity field
+    Ny = 32; Nz = 32; Nt = 32
+    y = chebpts(Ny)
+    Dy = chebdiff(Ny)
+    Dy2 = chebddiff(Ny)
+    ws = rand(Float64, Ny)
+    ω = 1.0
+    β = 1.0
+    grid = Grid(y, Nz, Nt, Dy, Dy2, ws, ω, β)
+    u_fun(y, z, t) = sin(π*y)*exp(cos(z))*sin(t)
+    v_fun(y, z, t) = (cos(π*y) + 1)*cos(z)*sin(t)
+    w_fun(y, z, t) = π*sin(π*y)*sin(z)*sin(t)
+    u = VectorField(PhysicalField(grid, u_fun),
+                    PhysicalField(grid, v_fun),
+                    PhysicalField(grid, w_fun))
+    U = VectorField(grid)
+    FFT! = FFTPlan!(grid; flags=FFTW.ESTIMATE)
+    FFT!(U, u)
+    Re = abs(rand())
+    Ro = abs(rand())
+
+    # initialise cache and update velocity field
+    Re = abs(rand())
+    Ro = abs(rand())
+    cache = Cache(U[1], u[1], rand(Ny), rand(Ny), rand(Ny), rand(Ny), Re, Ro)
+    NSOperators._update_vel!(U, cache)
+
+    # initialise fields for results
+    dudt_fun(y, z, t) = sin(π*y)*exp(cos(z))*cos(t)
+    dvdt_fun(y, z, t) = (cos(π*y) + 1)*cos(z)*cos(t)
+    dwdt_fun(y, z, t) = π*sin(π*y)*sin(z)*cos(t)
+    dudz_fun(y, z, t) = -sin(π*y)*sin(z)*exp(cos(z))*sin(t)
+    dvdz_fun(y, z, t) = -(cos(π*y) + 1)*sin(z)*sin(t)
+    dwdz_fun(y, z, t) = π*sin(π*y)*cos(z)*sin(t)
+    d2udz2_fun(y, z, t) = sin(π*y)*((sin(z)^2) - cos(z))*exp(cos(z))*sin(t)
+    d2vdz2_fun(y, z, t) = -(cos(π*y) + 1)*cos(z)*sin(t)
+    d2wdz2_fun(y, z, t) = -π*sin(π*y)*sin(z)*sin(t)
+    dudy_fun(y, z, t) = π*cos(π*y)*exp(cos(z))*sin(t)
+    dvdy_fun(y, z, t) = -π*sin(π*y)*cos(z)*sin(t)
+    dwdy_fun(y, z, t) = (π^2)*cos(π*y)*sin(z)*sin(t)
+    d2udy2_fun(y, z, t) = -(π^2)*sin(π*y)*exp(cos(z))*sin(t)
+    d2vdy2_fun(y, z, t) = -(π^2)*cos(π*y)*cos(z)*sin(t)
+    d2wdy2_fun(y, z, t) = -(π^3)*sin(π*y)*sin(z)*sin(t)
+    v_dudy_fun(y, z, t) = v_fun(y, z, t)*dudy_fun(y, z, t)
+    w_dudz_fun(y, z, t) = w_fun(y, z, t)*dudz_fun(y, z, t)
+    v_dvdy_fun(y, z, t) = v_fun(y, z, t)*dvdy_fun(y, z, t)
+    w_dvdz_fun(y, z, t) = w_fun(y, z, t)*dvdz_fun(y, z, t)
+    v_dwdy_fun(y, z, t) = v_fun(y, z, t)*dwdy_fun(y, z, t)
+    w_dwdz_fun(y, z, t) = w_fun(y, z, t)*dwdz_fun(y, z, t)
+    # dvdz_dwdy_fun(y, z, t) = dvdz_fun(y, z, t)*dwdy_fun(y, z, t)
+    # dvdy_dwdz_fun(y, z, t) = dvdy_fun(y, z, t)*dwdz_fun(y, z, t)
+    dvdz_dwdy_fun(y, z, t) = -(π^2)*cos(π*y)*(cos(π*y) + 1)*(sin(z)^2)*(sin(t)^2)
+    dvdy_dwdz_fun(y, z, t) = -(π^2)*(sin(π*y)^2)*(cos(z)^2)*(sin(t)^2)
+    dudt = PhysicalField(grid, dudt_fun)
+    dvdt = PhysicalField(grid, dvdt_fun)
+    dwdt = PhysicalField(grid, dwdt_fun)
+    dudz = PhysicalField(grid, dudz_fun)
+    dvdz = PhysicalField(grid, dvdz_fun)
+    dwdz = PhysicalField(grid, dwdz_fun)
+    d2udz2 = PhysicalField(grid, d2udz2_fun)
+    d2vdz2 = PhysicalField(grid, d2vdz2_fun)
+    d2wdz2 = PhysicalField(grid, d2wdz2_fun)
+    dudy = PhysicalField(grid, dudy_fun)
+    dvdy = PhysicalField(grid, dvdy_fun)
+    dwdy = PhysicalField(grid, dwdy_fun)
+    d2udy2 = PhysicalField(grid, d2udy2_fun)
+    d2vdy2 = PhysicalField(grid, d2vdy2_fun)
+    d2wdy2 = PhysicalField(grid, d2wdy2_fun)
+    v_dudy = PhysicalField(grid, v_dudy_fun)
+    w_dudz = PhysicalField(grid, w_dudz_fun)
+    v_dvdy = PhysicalField(grid, v_dvdy_fun)
+    w_dvdz = PhysicalField(grid, w_dvdz_fun)
+    v_dwdy = PhysicalField(grid, v_dwdy_fun)
+    w_dwdz = PhysicalField(grid, w_dwdz_fun)
+    dvdz_dwdy = PhysicalField(grid, dvdz_dwdy_fun)
+    dvdy_dwdz = PhysicalField(grid, dvdy_dwdz_fun)
+    dUdt = SpectralField(grid)
+    dVdt = SpectralField(grid)
+    dWdt = SpectralField(grid)
+    dUdz = SpectralField(grid)
+    dVdz = SpectralField(grid)
+    dWdz = SpectralField(grid)
+    d2Udz2 = SpectralField(grid)
+    d2Vdz2 = SpectralField(grid)
+    d2Wdz2 = SpectralField(grid)
+    dUdy = SpectralField(grid)
+    dVdy = SpectralField(grid)
+    dWdy = SpectralField(grid)
+    d2Udy2 = SpectralField(grid)
+    d2Vdy2 = SpectralField(grid)
+    d2Wdy2 = SpectralField(grid)
+    V_dUdy = SpectralField(grid)
+    W_dUdz = SpectralField(grid)
+    V_dVdy = SpectralField(grid)
+    W_dVdz = SpectralField(grid)
+    V_dWdy = SpectralField(grid)
+    W_dWdz = SpectralField(grid)
+    dVdz_dWdy = SpectralField(grid)
+    dVdy_dWdz = SpectralField(grid)
+    FFT!(dUdt, dudt)
+    FFT!(dVdt, dvdt)
+    FFT!(dWdt, dwdt)
+    FFT!(dUdz, dudz)
+    FFT!(dVdz, dvdz)
+    FFT!(dWdz, dwdz)
+    FFT!(d2Udz2, d2udz2)
+    FFT!(d2Vdz2, d2vdz2)
+    FFT!(d2Wdz2, d2wdz2)
+    FFT!(dUdy, dudy)
+    FFT!(dVdy, dvdy)
+    FFT!(dWdy, dwdy)
+    FFT!(d2Udy2, d2udy2)
+    FFT!(d2Vdy2, d2vdy2)
+    FFT!(d2Wdy2, d2wdy2)
+    FFT!(V_dUdy, v_dudy)
+    FFT!(W_dUdz, w_dudz)
+    FFT!(V_dVdy, v_dvdy)
+    FFT!(W_dVdz, w_dvdz)
+    FFT!(V_dWdy, v_dwdy)
+    FFT!(W_dWdz, w_dwdz)
+    FFT!(dVdz_dWdy, dvdz_dwdy)
+    FFT!(dVdy_dWdz, dvdy_dwdz)
+
+    # check if they agree
+    @test dUdt ≈ cache.spec_cache[1]
+    @test dVdt ≈ cache.spec_cache[2]
+    @test dWdt ≈ cache.spec_cache[3]
+    @test dUdz ≈ cache.spec_cache[4]
+    @test dVdz ≈ cache.spec_cache[5]
+    @test dWdz ≈ cache.spec_cache[6]
+    @test d2Udz2 ≈ cache.spec_cache[7]
+    @test d2Vdz2 ≈ cache.spec_cache[8]
+    @test d2Wdz2 ≈ cache.spec_cache[9]
+    @test dUdy ≈ cache.spec_cache[10]
+    @test dVdy ≈ cache.spec_cache[11]
+    @test dWdy ≈ cache.spec_cache[12]
+    @test d2Udy2 ≈ cache.spec_cache[13]
+    @test d2Vdy2 ≈ cache.spec_cache[14]
+    @test d2Wdy2 ≈ cache.spec_cache[15]
+    @test V_dUdy ≈ cache.spec_cache[20]
+    @test W_dUdz ≈ cache.spec_cache[21]
+    @test V_dVdy ≈ cache.spec_cache[22]
+    @test W_dVdz ≈ cache.spec_cache[23]
+    @test V_dWdy ≈ cache.spec_cache[24]
+    @test W_dWdz ≈ cache.spec_cache[25]
+    @test dVdz_dWdy ≈ cache.spec_cache[26]
+    @test dVdy_dWdz ≈ cache.spec_cache[27]
+end
+
+@testset "Cache pressure update         " begin
+    # initialise incompressible velocity field
+    Ny = 32; Nz = 32; Nt = 32
+    y = chebpts(Ny)
+    Dy = chebdiff(Ny)
+    Dy2 = chebddiff(Ny)
+    ws = rand(Float64, Ny)
+    ω = 1.0
+    β = 1.0
+    grid = Grid(y, Nz, Nt, Dy, Dy2, ws, ω, β)
+    u_fun(y, z, t) = sin(π*y)*exp(cos(z))*sin(t)
+    v_fun(y, z, t) = (cos(π*y) + 1)*cos(z)*sin(t)
+    w_fun(y, z, t) = π*sin(π*y)*sin(z)*sin(t)
+    u = VectorField(PhysicalField(grid, u_fun),
+                    PhysicalField(grid, v_fun),
+                    PhysicalField(grid, w_fun))
+    U = VectorField(grid)
+    FFT! = FFTPlan!(grid; flags=FFTW.ESTIMATE)
+    FFT!(U, u)
+
+    # initialise cache and update pressure field
+    Re = abs(rand()); Ro = abs(rand())
+    cache = Cache(U[1], u[1], rand(Ny), rand(Ny), rand(Ny), rand(Ny), Re, Ro)
+    NSOperators._update_vel!(U, cache)
+    NSOperators._update_p!(U, cache)
+
+    # compute laplacian of pressure field
+    P = cache.spec_cache[16]
+    d2Pdy2 = SpectralField(grid)
+    d2Pdz2 = SpectralField(grid)
+    ΔP = SpectralField(grid)
+    d2dy2!(P, d2Pdy2)
+    d2dz2!(P, d2Pdz2)
+    ΔP .= d2Pdy2 .+ d2Pdz2
+
+    # initialise rhs field
+    # NOTE: the laplacian of pressure and rhs nonlinear terms do not individually match (at mean spanwise mode), but collectively (their difference) they do. why??? 
+    rhs_fun(y, z, t) = 2*(π^2)*(sin(t)^2)*(cos(π*y)*(cos(π*y) + 1)*(sin(z)^2) - (sin(π*y)^2)*(cos(z)^2)) - Ro*π*cos(π*y)*exp(cos(z))*sin(t)
+    rhs_phys = PhysicalField(grid, rhs_fun)
+    rhs = SpectralField(grid)
+    FFT!(rhs, rhs_phys)
+
+    @test ΔP ≈ rhs
+end
